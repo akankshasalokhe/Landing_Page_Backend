c4e44c968520676768edd2cd089a7789f05b6511
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-07-25 17:56:57 +0530

    update

--- api/index.js
+++ api/index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Handle large payloads
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+
 // If you serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
